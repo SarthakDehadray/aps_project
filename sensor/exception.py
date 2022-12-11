7c9de1a7d561ef964a16bdc77d736e63657a5307
Sarthak Dehadray <sarthakdehadray@gmail.com>
2022-12-11 08:18:13 +0530

    Added requisite files and folders

--- sensor/exception.py
+++ sensor/exception.py
@@ -6,8 +6,8 @@ def error_message_detail(error, error_detail: sys):
     error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
-
     return error_message
+
 
 
 class SensorException(Exception):
